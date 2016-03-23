5fec806e9a9861f888776da43e31646d5d745af9
Renato Utsch <renatoutsch@gmail.com>
2016-03-23 00:02:37 -0300

    Update renato.cpp
    Atualizar minha implementação do ELEICOES porque eu quis huehue

--- roteiro02/spojbr_eleicoes/renato.cpp
+++ roteiro02/spojbr_eleicoes/renato.cpp
@@ -1,23 +1,35 @@
-#include <iostream>
+/**
+ * Esse problema foi resolvido através de usar uma árvore binária
+ * para armazenar os votos, e iterar pela árvore binária para encontrar
+ * o concorrente com mais votos. Isso funciona pois é necessário iterar
+ * apenas pelo número de candidatos participantes, e não pelo número de
+ * votos.
+ * Foi utilizada a classe std::map, que é a implementação da STL de uma
+ * árvore binária (normalmente uma Red-Black Tree).
+ */
+
 #include <map>
-using namespace std;
+#include <iostream>
 
 int main() {
-    std::map<int, int> votes;
-    int n, input;
+    std::ios_base::sync_with_stdio(false);
+    std::map<int, int> db;
+    int votes, num;
 
-    cin >> n;
-    for(int i = 0; i < n; ++i) {
-        cin >> input;
-        votes[input] += 1;
+    std::cin >> votes;
+    while(votes--) {
+        std::cin >> num;
+        ++db[num];
     }
 
-    std::map<int, int>::iterator winner, it;
-    for(it = votes.begin(), winner = votes.begin(); it != votes.end(); ++it) {
-        if(it->second > winner->second)
-            winner = it;
+    typedef std::map<int, int>::iterator It;
+    It biggest = db.begin();
+    for(It it = ++db.begin(); it != db.end(); ++it) { // assumes one vote.
+        if(it->second > biggest->second)
+            biggest = it;
     }
 
-    cout << winner->first << "\n";
+    std::cout << biggest->first;
+
     return 0;
 }
