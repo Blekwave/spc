c5490bbf45c57b5a4164480dec453125cdbf770b
Lucas Augusto <luc.aug.freire@gmail.com>
2016-04-09 20:03:38 -0300

    Tentando resolver o problema UVa_11060, sem sucesso até agora

---
+++ roteiro05/UVa_11060/lucas.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <queue>
+#include <list>
+#include <map>
+
+int main() {
+    std::string name, nameRequired;
+    int N, M, nTest = 1;
+
+    while(std::cin >> N) {
+        if(nTest != 1) std::cout << "\n";
+
+        std::map<std::string, std::list<std::string> > adjList;
+        std::vector<std::string> names;
+        std::queue<std::string> zeroInDegree, result;
+
+        for(int i = 0; i < N; i++) {
+            std::cin >> name;
+            adjList[name] = std::list<std::string>();
+            names.push_back(name);
+        }
+        std::cin >> M;
+        for(int i = 0; i < M; i++) {
+            std::cin >> nameRequired >> name;
+            adjList[name].push_back(nameRequired);
+        }
+
+        for(int i = 0; i < N; i++) {
+            auto elem = adjList[names[i]];
+            if(elem.empty()) {
+                zeroInDegree.push(names[i]);
+                adjList.erase(names[i]);
+            }
+        }
+
+        while(!zeroInDegree.empty()) {
+            name = zeroInDegree.front();
+            zeroInDegree.pop();
+
+            result.push(name);
+
+            for(int i = 0; i < N; i++) {
+                auto currBev = adjList[names[i]];
+                if(currBev.empty()) continue;
+                for(auto req = currBev.begin(); req != currBev.end(); req++) {
+                    std::cout << "Requisito " << *req << " de " << names[i] << ": " << name << "\n";
+                    if(*req == name) {
+                        std::cout << "req == name\n";
+                        currBev.erase(req);
+                        break;
+                    }
+                }
+                if(currBev.empty()) {
+                    zeroInDegree.push(names[i]);
+                    adjList.erase(names[i]);
+                }
+            }
+        }
+
+        std::cout << "Case #" << nTest << ":  Dilbert should drink beverages in this order:";
+        while(!result.empty()) {
+            std::cout << " " << result.front();
+            result.pop();
+        }
+        std::cout << ".\n\n";
+
+        nTest++;
+    }
+
+    return 0;
+}
