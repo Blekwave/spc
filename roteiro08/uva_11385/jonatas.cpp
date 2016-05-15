9d1ac2f3cba8804448542e0c5b5eb600901fce20
Lucas Augusto <luc.aug.freire@gmail.com>
2016-05-15 16:01:27 -0300

    uva_11385 (jonatas)

---
+++ roteiro08/uva_11385/jonatas.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int const QTD_FIBONACCI_NUMBERS = 45;
+int const MAX_CYPHER_SIZE = 100;
+
+std::vector<int> fillFibonacciVector(int qtd_numbers);
+void decodeText(std::vector<int> inputNumbers, std::vector<int> fibonacciNumbers, std::string cypherText);
+int getFibonacciIndex(int number, std::vector<int> fibonacciNumbers);
+
+int main()  {
+
+	int T, N, input;
+	std::vector<int> inputNumbers;
+	std::vector<int> fibonacciNumbers(QTD_FIBONACCI_NUMBERS);
+	std::string cypherText;
+
+	fibonacciNumbers = fillFibonacciVector(QTD_FIBONACCI_NUMBERS);
+
+	std::cin >> T;
+
+	while(T--)  {
+
+		std::cin >> N;
+		
+		while(N--)  {
+			std::cin >> input;
+			inputNumbers.push_back(input);
+		}
+
+		std::cin.ignore();
+		std::getline(std::cin, cypherText);
+		decodeText(inputNumbers, fibonacciNumbers, cypherText);
+		std::cout << "\n";
+		inputNumbers.clear();
+	}
+	
+	return 0;
+}
+
+std::vector<int> fillFibonacciVector(int qtd_numbers)  {
+	
+	int first = 1,
+		second = 1,
+		next;
+	std::vector<int> fibonacciNumbers;
+
+	for(int i = 1; i < qtd_numbers+1; i++)  {
+
+		if(i <= 1) 
+			next = i;
+		else  {
+			next = first + second;
+			first = second;
+			second = next;
+		}
+
+		fibonacciNumbers.push_back(next);
+	}
+
+	return fibonacciNumbers;
+}
+
+void decodeText(std::vector<int> inputNumbers, std::vector<int> fibonacciNumbers, std::string cypherText)  {
+
+	char output[MAX_CYPHER_SIZE],
+		 character;
+	int fibonacciIndex = 0,
+		maxFibonacciIndex = 0,
+		lastValidIndex = 0;
+	bool usedFibonacciNumbers[QTD_FIBONACCI_NUMBERS];
+	
+	for(int i = 0; i < QTD_FIBONACCI_NUMBERS; i++)
+		usedFibonacciNumbers[i] = false;
+
+	for(int i = 0; i < inputNumbers.size(); i++)  {
+
+		fibonacciIndex = getFibonacciIndex(inputNumbers[i], fibonacciNumbers);
+		usedFibonacciNumbers[fibonacciIndex] = true; 
+
+		if(fibonacciIndex > maxFibonacciIndex)
+			maxFibonacciIndex = fibonacciIndex;
+
+		for(int j = lastValidIndex; j < cypherText.size(); j++)  {
+			if((int)cypherText[j] >= 65 && (int)cypherText[j] <= 90)  {
+				character = cypherText[j];
+				lastValidIndex = j+1;
+				break;
+			}
+		}
+
+		output[fibonacciIndex] = character;
+	}
+
+	for(int i = 0; i < maxFibonacciIndex + 1; i++)  {
+		if(!usedFibonacciNumbers[i])
+			output[i] = (char)32;
+	}
+
+	for(int i = 0; i < maxFibonacciIndex + 1; i++)  {
+		std::cout<<output[i];
+	}
+}
+
+int getFibonacciIndex(int number, std::vector<int> fibonacciNumbers)  {
+	
+	for(int i = 0; i < QTD_FIBONACCI_NUMBERS; i++)  {
+		if(number == fibonacciNumbers[i])
+			return i;
+	}
+	return 0;
+}
