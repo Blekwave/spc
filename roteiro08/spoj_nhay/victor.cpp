58cd9d292c692893a8fd249ff222d033a7f22ab8
Victor Diniz <blek.net@gmail.com>
2016-05-17 22:05:09 -0300

    spoj_nhay pronto

--- roteiro08/spoj_nhay/victor.cpp
+++ roteiro08/spoj_nhay/victor.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
 #include <vector>
 #include <algorithm>
-#include <limits>
 
-using std::min;
-using std::cin;
-using std::cout;
-using std::vector;
-using std::string;
+using namespace std;
 
-int prefixIsSuffix(string needle, int size, int end){
-    for(int i = 0, j = end - size; i < size; i++, j++){
+int prefixIsSuffix(char *needle, int size, int end){
+    for (int i = 0, j = end - size; i < size; i++, j++){
         if (needle[i] != needle[j]){
             return false;
         }
@@ -19,12 +15,12 @@ int prefixIsSuffix(string needle, int size, int end){
     return true;
 }
 
-vector<int> partialMatchTable(string needle){
-    vector<int> pmt(needle.size() + 1);
+vector<int> partialMatchTable(int n, char * needle){
+    vector<int> pmt(n + 1);
     pmt[0] = -1;
     pmt[1] = 0;
-    for (int i = 1; i < needle.size(); i++){
-        int max_prefix = min(needle.size() - 1, pmt[i]);
+    for (int i = 1; i < n; i++){
+        int max_prefix = min(n - 1, pmt[i] + 1);
         int best_valid_prefix = 0;
         for (int prefix_size = max_prefix; prefix_size > 0; prefix_size--){
             if (prefixIsSuffix(needle, prefix_size, i + 1)){
@@ -37,34 +33,43 @@ vector<int> partialMatchTable(string needle){
 }
 
 void runCase(int n){
-    string needle;
-    cin >> needle;
-    vector<int> pmt = partialMatchTable(needle);
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), "\n");
+    char *needle = (char *) malloc(n + 1);
+    getchar();
+    fgets(needle, n + 1, stdin);
+    getchar();
+    vector<int> pmt = partialMatchTable(n, needle);
     char c;
     int i = 0;
     int j = 0;
-    int matches = 0;
     while (true){
-        cin.get(c);
-        j++;
+        c = getchar();
         if (c == '\n'){
             break;
         }
-        if (needle[i] == c){
-            i++;
-            if (i == needle.size()){
-                cout << j << "\n";
-                i =
+        /* printf("i%d j%d c%c\n", i, j, c); */
+        while (true){
+            if (c == needle[j]){
+                j++;
+                if (j == n){
+                    printf("%d\n", i - j + 1);
+                    j = pmt[j];
+                }
+                break;
+            } else if (j == 0) {
+                break;
+            } else {
+                j = pmt[j];
             }
         }
+        i++;
     }
-    cout << "\n";
+    printf("\n");
+    free(needle);
 }
 
 int main(){
     int n;
-    while (cin >> n){
+    while (scanf("%d", &n) && !feof(stdin)){
         runCase(n);
     }
     return 0;
